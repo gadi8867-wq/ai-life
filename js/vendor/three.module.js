7d8d8c45d97ea1b4036912355f3f18ae5b1eb03d
gadi8867-wq <gadi8867@gmail.com>
2026-09-17 18:08:30 +0300

    fix: make local Three.js module load reliably

--- js/vendor/three.module.js
+++ js/vendor/three.module.js
@@ -1,1 +1,1 @@
-/* Three.js vendor file placeholder: deployment fix will be completed from the verified upstream release. */
+export * from 'https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js';
